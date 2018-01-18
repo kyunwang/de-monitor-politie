b0765d9f31fe36ee621cc6cd6832391fc773b956
IIYAMA12 <jorik@MBP-van-jw.lan>
2018-01-18 22:06:44 +0100

    structure of website

--- scripts/main.js
+++ scripts/main.js
@@ -14,3 +14,27 @@ function goToNextArticle () {
 goToNextArticle ();
 goToNextArticle ();
 goToNextArticle ();
+goToNextArticle ();
+
+
+
+function setFocusPoint (article, x, y) {
+    if (article != undefined) {
+        var overlayElement = article.querySelector(".overlay");
+        if (overlayElement) {
+            var focusPoint_svg = overlayElement.querySelector("svg");
+            if (x == undefined) {
+                x = 0;
+            }
+            if (y == undefined) {
+                y = 0;
+            }
+            focusPoint_svg.style.transform = "translate(" + x + "px, " + y + "px) scale(2)";
+            
+        }
+    }
+}
+
+setTimeout(function () {
+    setFocusPoint(allArticles[4], 100, 100);
+}, 500);
