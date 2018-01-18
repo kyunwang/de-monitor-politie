86f1eff91740e9d8ed1ea5ac04909c8628418f7e
Kangyun Wang <wky@150.150.028.145.hva.nl>
2018-01-18 15:46:17 +0100

    Fixed assets.working on bubble chart

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-
 var mainElement = document.getElementsByTagName("main")[0];
 
 var articleContainer = document.getElementById("article-container");
@@ -6,11 +5,11 @@ var articleContainer = document.getElementById("article-container");
 var allArticles = articleContainer.querySelectorAll("article");
 var articleIndex = 0;
 
-function goToNextArticle () {
-    articleIndex++;
-    articleContainer.style.left = (-100 * articleIndex) + "vw";
+function goToNextArticle() {
+  articleIndex++;
+  articleContainer.style.left = -100 * articleIndex + "vw";
 }
 
-goToNextArticle ();
-goToNextArticle ();
-goToNextArticle ();
+goToNextArticle();
+// goToNextArticle ();
+// goToNextArticle ();
