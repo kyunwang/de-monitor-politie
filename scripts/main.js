a5a344628d72654a31f82d10064ff5bd2df0d7d1
IIYAMA12 <jorik@197.226.028.145.hva.nl>
2018-01-18 13:46:57 +0100

    Eerste versie structuur website

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,16 @@
-var allArticles = document.querySelectorAll("main > article");
+
+var mainElement = document.getElementsByTagName("main")[0];
+
+var articleContainer = document.getElementById("article-container");
+
+var allArticles = articleContainer.querySelectorAll("article");
 var articleIndex = 0;
 
 function goToNextArticle () {
-
+    articleIndex++;
+    articleContainer.style.left = (-100 * articleIndex) + "vw";
 }
+
+goToNextArticle ();
+goToNextArticle ();
+goToNextArticle ();
