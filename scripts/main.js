ea89c3297a181b368442fdd6248868b49cbaf29a
IIYAMA12 <jorik@MacBook-Pro-van-jw.local>
2018-01-19 12:55:04 +0100

    style applying

--- scripts/main.js
+++ scripts/main.js
@@ -3,18 +3,47 @@ var mainElement = document.getElementsByTagName("main")[0];
 
 var articleContainer = document.getElementById("article-container");
 
-var allArticles = articleContainer.querySelectorAll("article");
+var articles = articleContainer.querySelectorAll("article");
+
+
+// 
+
+var articlesById = {};
+for (let index = 0; index < articles.length; index++) {
+    const article = articles[index];
+    
+    var id = article.id;
+    if (id != undefined) {
+        articlesById[id] = article;
+    }
+}
+
+// 
+
 var articleIndex = 0;
 
 function goToNextArticle () {
     articleIndex++;
-    articleContainer.style.left = (-100 * articleIndex) + "vw";
+    if (articles[articleIndex] != undefined) {
+        articleContainer.style.left = (-100 * articleIndex) + "vw";
+        return articles[articleIndex];
+    } else {
+        articleIndex--;
+    }
+    return false;
 }
 
-goToNextArticle ();
-goToNextArticle ();
-goToNextArticle ();
-goToNextArticle ();
+function goToPreviousArticle () {
+    articleIndex--;
+    if (articles[articleIndex] != undefined) {
+        articleContainer.style.left = (-100 * articleIndex) + "vw";
+        return  articles[articleIndex];
+    } else {
+        articleIndex++;
+    }
+    return false;
+}
+
 
 
 
@@ -30,11 +59,45 @@ function setFocusPoint (article, x, y) {
                 y = 0;
             }
             focusPoint_svg.style.transform = "translate(" + x + "px, " + y + "px) scale(2)";
-            
+            return true;
         }
     }
+    return false;
 }
 
+/////////////////
+//  test stuff //
+
+
 setTimeout(function () {
-    setFocusPoint(allArticles[4], 100, 100);
-}, 500);
+    // goToNextArticle ();
+    // goToNextArticle ();
+    // goToNextArticle ();
+    // goToNextArticle ();
+}, 300);
+
+setTimeout(function () {
+    setFocusPoint(articles[4], 100, 100);
+}, 1500);
+
+
+//  test stuff //
+/////////////////
+
+
+/////////////////////////////
+// switch between articles //
+
+
+var buttonsToNextArticle = document.getElementsByClassName("button--go-to-next-article");
+
+
+
+for (let index = 0; index < buttonsToNextArticle.length; index++) {
+    const button = buttonsToNextArticle[index];
+    button.addEventListener("click", goToNextArticle);
+}
+
+
+// switch between articles //
+/////////////////////////////
