3fca76fde669ee855f8f2c04ab30e4255ce46cb0
IIYAMA12 <jorik@044.162.028.145.hva.nl>
2018-01-24 11:55:01 +0100

    Management system v1

--- scripts/management.js
+++ scripts/management.js
@@ -3,6 +3,8 @@ var articleBehaviourData = {
         setTimeout(goToNextArticle, 1500);
     },
     ["route-2"] : function (article) {
+        setFocusPoint(article);
+
         var infoBalloonWrapper = article.getElementsByClassName("wrapper--info-balloon")[0];
 
         var foreword = infoBalloonWrapper.getElementsByClassName("foreword")[0];
@@ -10,10 +12,10 @@ var articleBehaviourData = {
 
         var instruction = infoBalloonWrapper.getElementsByClassName("instruction")[0];
 
-        var feedback = infoBalloonWrapper.getElementsByClassName("feedback")[0];
+        // var feedback = infoBalloonWrapper.getElementsByClassName("feedback")[0];
         
 
-        setFocusPoint(article);
+        
         
         var goToNextStep = document.getElementById("go-to-next-step");
         
@@ -29,20 +31,107 @@ var articleBehaviourData = {
             // remove foreword and show instruction
             foreword.classList.add("disabled");
             instruction.classList.remove("disabled");
+
             goToNextStep.classList.add("hidden");
-            var recognizeAbleOptions = article.querySelector(".overlay-effect span");
-            if(recognizeAbleOptions != undefined) {
-                recognizeAbleOptions.classList.add("active");
-            }
+
+            // Show buttons "herkenbaar" and "onherkenbaar"
+            setRecognizAbleButtonsStatus(article, true);
         };
 
         
         goToNextStep.addEventListener("click", showInstruction);
-    }
+    },
+    ["house"] : function (article) {
+        setFocusPoint(article);
+
+        var infoBalloonWrapper = article.getElementsByClassName("wrapper--info-balloon")[0];
+
+        var instruction = infoBalloonWrapper.getElementsByClassName("instruction")[0];
+        
+        instruction.classList.remove("disabled");
+
+        // Show buttons "herkenbaar" and "onherkenbaar"
+        setRecognizAbleButtonsStatus(article, true);
+    }, 
+    ["house-num"] : function (article) {
+        setFocusPoint(article);
+
+        var infoBalloonWrapper = article.getElementsByClassName("wrapper--info-balloon")[0];
+        console.log(infoBalloonWrapper);
+        
+        var instruction = infoBalloonWrapper.getElementsByClassName("instruction")[0];
+        
+        instruction.classList.remove("disabled");
+
+        // Show buttons "herkenbaar" and "onherkenbaar"
+        setRecognizAbleButtonsStatus(article, true);
+    },
+    ["open-door"] : function (article) {
+        setTimeout(goToNextArticle, 1500);
+    },
+    ["throw-beer"] : function (article) {
+        setFocusPoint(article);
+
+        var infoBalloonWrapper = article.getElementsByClassName("wrapper--info-balloon")[0];
+        console.log(infoBalloonWrapper);
+        
+        var instruction = infoBalloonWrapper.getElementsByClassName("instruction")[0];
+        
+        instruction.classList.remove("disabled");
+
+        // Show buttons "herkenbaar" and "onherkenbaar"
+        setRecognizAbleButtonsStatus(article, true);
+    },
+    ["license-plate"] : function (article) {
+        setFocusPoint(article);
+
+        var infoBalloonWrapper = article.getElementsByClassName("wrapper--info-balloon")[0];
+        console.log(infoBalloonWrapper);
+        
+        var instruction = infoBalloonWrapper.getElementsByClassName("instruction")[0];
+        
+        instruction.classList.remove("disabled");
+
+        // Show buttons "herkenbaar" and "onherkenbaar"
+        setRecognizAbleButtonsStatus(article, true);
+    },
+    ["neighbor"] : function (article) {
+        setFocusPoint(article);
+
+        var infoBalloonWrapper = article.getElementsByClassName("wrapper--info-balloon")[0];
+        console.log(infoBalloonWrapper);
+        
+        var instruction = infoBalloonWrapper.getElementsByClassName("instruction")[0];
+        
+        instruction.classList.remove("disabled");
+
+        // Show buttons "herkenbaar" and "onherkenbaar"
+        setRecognizAbleButtonsStatus(article, true);
+    },
+    ["route-3"] : function (article) {
+        setTimeout(goToNextArticle, 1500);
+    },
+    ["police-desk"] : function (article) {
+        setTimeout(goToNextArticle, 1500);
+    },
+    ["uploading"] : function (article) {
+        setTimeout(goToNextArticle, 1500);
+    },
 };
 
 var canUseNextButton = true;
 
+// Show/hide buttons "herkenbaar" and "onherkenbaar"
+function setRecognizAbleButtonsStatus (article, status) {
+    var recognizeAbleOptions = article.querySelector(".overlay-effect span");
+    if(recognizeAbleOptions != undefined) {
+        if (status) {
+            recognizeAbleOptions.classList.add("active");
+        } else {
+            recognizeAbleOptions.classList.remove("active");
+        }
+    }
+}
 
 function onSwitchArticle (article) {
     var id = article.id;
